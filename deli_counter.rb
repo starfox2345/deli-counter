df79a1e8464182e6cbc6cff283fa8b6d61ed1353
yatsun <yatsunlam25@gmail.com>
2021-03-01 00:01:10 -0500

    commit tree cd21cc18e8d5e7ac4a4055b2b55e115f93c361bd

--- deli_counter.rb
+++ deli_counter.rb
@@ -30,6 +30,7 @@ end
 def now_serving(katz_deli)
     if katz_deli.size == 0
         puts "There is nobody waiting to be served!"
+      
     else
         puts "Currently serving #{katz_deli.first}."
         katz_deli.shift
